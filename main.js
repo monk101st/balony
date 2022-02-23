5563d531ba7e12aa733dfb8c0ddfa6331c680b5f
Maciej Jaszczuk <git@artprotect.pl>
2022-02-23 18:11:57 +0100

    Insert code to main.js

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("main js");
+const header = document.querySelector("header");
+const h1 = document.querySelector("h1");
+let image = true;
+let title = true;
+
+
+const changeImage = () => {
+    image = !image;
+    title = !title;
+    header.style.backgroundImage = image ? "url(sw-001.jpg)" : "url(sw-002.jpg)";
+    h1.textContent = title ? "Baloon" : "Dupa"
+}
+
+header.addEventListener("click", changeImage);
